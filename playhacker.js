a13669e7c5b25648fbf9e4133c6067009c0971e6
Haider <haidersoft2012@gmail.com>
2020-04-25 21:30:59 +0530

    comment style change

--- playhacker.js
+++ playhacker.js
@@ -1,4 +1,4 @@
-/***snippet-Bookmarks button styling***/ 
+/*======snippet-Bookmarks button styling=====*/ 
 jQuery(document).ready(function($){
 				$( "i.bookmark" ).click(function() {
 				  $( this ).toggleClass( "highlight" );
