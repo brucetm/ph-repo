9c130814065872b09cedab541ffcd23c608f723a
Haider <haidersoft2012@gmail.com>
2020-04-26 21:42:45 +0530

    Snippet Bookmark: add button to product page

---
+++ ph-functions.php
@@ -0,0 +1,8 @@
+//Add favorite button on single product page:
+add_action( 'woocommerce_after_add_to_cart_button', 'ph_favorite_before_add_to_cart_btn' );
+ 
+function ph_favorite_before_add_to_cart_btn(){
+	echo '<p class="fav_btn">';
+	echo do_shortcode('[favorite_button]');
+	echo '</p>';
+}
