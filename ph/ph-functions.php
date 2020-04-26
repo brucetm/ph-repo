99829a87bd0c29f604e8894c2d94ba90f324e369
Haider <haidersoft2012@gmail.com>
2020-04-26 22:35:32 +0530

    Added php tag

--- ph/ph-functions.php
+++ ph/ph-functions.php
@@ -1,3 +1,4 @@
+<?php
 /**=============Snippet:- Bookmark: add button to product page=============*/
 //Add favorite button on single product page:
 add_action( 'woocommerce_after_add_to_cart_button', 'ph_favorite_before_add_to_cart_btn' );
@@ -7,3 +8,4 @@ function ph_favorite_before_add_to_cart_btn(){
 	echo do_shortcode('[favorite_button]');
 	echo '</p>';
 }
+?>
