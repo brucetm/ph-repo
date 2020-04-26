b8471732929611831907dc3cf4d9c757febb2fc7
Haider <haidersoft2012@gmail.com>
2020-04-26 21:53:27 +0530

    include global custom file ph-functions.php

--- ph-functions.php
+++ ph-functions.php
@@ -1,3 +1,4 @@
+/**=============Snippet:- Bookmark: add button to product page=============*/
 //Add favorite button on single product page:
 add_action( 'woocommerce_after_add_to_cart_button', 'ph_favorite_before_add_to_cart_btn' );
  
