39934e14ed27d959545b62d6c7f57de6c77d5479
Haider <haidersoft2012@gmail.com>
2020-05-01 01:11:38 +0530

    child theme push on github

--- buddyboss-theme-child/assets/js/playhacker-footer.js
+++ buddyboss-theme-child/assets/js/playhacker-footer.js
@@ -23,3 +23,50 @@
 				}, 1000);
 		});	 
 		
+/*======Snippet:-Flask tweaks======*/
+/* Flask plugin is only enabled under certain conditions: */
+			jQuery(document).ready(function(){
+				
+				//console.log("Flask snippet");
+
+				var windowHeight = jQuery(window).height(); 
+            	var windowWidth = jQuery(window).width(); 
+        		//console.log("windowHeight= "+windowHeight+" windowWidth= "+windowWidth);
+				var blockIt = 0;
+				var mobileStatus = "big enough";
+				var elementorStatus = "not editing";
+				var pageStatus = "not blacklisted";
+
+    			/* If screen is too small (check both dims): */
+				if ((windowHeight < 768) || (windowWidth < 768)) {
+					++blockIt;
+					mobileStatus = "too small";
+				}	
+				
+				/* If inside Elementor editor: */ 
+				if (jQuery('body').hasClass("elementor-editor-active")) {
+					++blockIt;
+					elementorStatus = "in editor";
+				}						
+				
+				/* Get current page name and window size: */
+				var currPostTitle = jQuery(document).attr('title');
+        		//console.log(currPostTitle);
+
+				/* Make an array of no-flask pages: */
+				/* NOTE: BuddyBoss hard codes endashes, which are not hyphens, so I copy pasted these from wikipedia: */
+				var disallowedPages = jQuery.inArray( currPostTitle, [ "Create an Account – playhacker!", "Activate Your Account – playhacker!", "coming soon – playhacker!" ] );
+
+				/* If an excluded page: */
+				if ( disallowedPages > -1 ) {
+					++blockIt;
+					pageStatus = "disallowed page";
+				}
+
+				if (blockIt > 0) {
+					jQuery("#flask").css("visibility", "hidden");
+				}
+				
+				//console.log("Flask status for "+currPostTitle+" is "+mobileStatus+", "+elementorStatus+", "+pageStatus+", disallowed="+disallowedPages);
+
+			});
